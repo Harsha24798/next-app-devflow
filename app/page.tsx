6b9183ba21ae1630a688ca0cdef15be68f5fd985
Harsha Madushan <69555530+Harsha24798@users.noreply.github.com>
2025-12-03 14:03:47 +0530

    Add local font import and apply

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-3xl text-violet-500'>Welcome to Next.js</div>
+    <div className='text-3xl text-violet-500 test-class'>Welcome to Next.js</div>
   )
 }
 
