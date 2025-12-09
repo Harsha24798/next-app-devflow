be2b6e2225179b3d0d90f81d7e12dc14b6669402
Harsha Madushan <69555530+Harsha24798@users.noreply.github.com>
2025-12-09 12:17:52 +0530

    Add Google provider to authentication and update sign-in button functionality

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,28 +1,33 @@
-'use client'
+"use client";
 
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { Icons } from "@/components/icons"
-import { toast } from "sonner"
-import ROUTES from "@/constants/routes"
-import { signIn } from "next-auth/react"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Icons } from "@/components/icons";
+import { toast } from "sonner";
+import ROUTES from "@/constants/routes";
+import { signIn } from "next-auth/react";
 
 const SignIn = () => {
-
   const handelSignIn = async (provider: "github" | "google") => {
     try {
-      await signIn(provider, { 
+      await signIn(provider, {
         callbackUrl: ROUTES.HOME,
         redirect: true,
-      })
+      });
     } catch (error) {
-      console.log(error)
-      toast.error("Something went wrong. Please try again.")
+      console.log(error);
+      toast.error("Something went wrong. Please try again.");
     }
-  }
+  };
 
   return (
     <div className="min-h-screen flex items-center justify-center px-4 absolute">
@@ -35,11 +40,19 @@ const SignIn = () => {
         </CardHeader>
         <CardContent>
           <div className="grid grid-cols-2 gap-4 mb-6">
-            <Button variant="outline" className="w-full" onClick={() => handelSignIn("github")}>
+            <Button
+              variant="outline"
+              className="w-full"
+              onClick={() => handelSignIn("github")}
+            >
               <Icons.github className="mr-2 h-4 w-4" />
               GitHub
             </Button>
-            <Button variant="outline" className="w-full">
+            <Button
+              variant="outline"
+              className="w-full"
+              onClick={() => handelSignIn("google")}
+            >
               <Icons.google className="mr-2 h-4 w-4" />
               Google
             </Button>
@@ -50,7 +63,9 @@ const SignIn = () => {
               <span className="w-full border-t" />
             </div>
             <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground pb-6">Or continue with email</span>
+              <span className="bg-background px-2 text-muted-foreground pb-6">
+                Or continue with email
+              </span>
             </div>
           </div>
 
@@ -71,14 +86,17 @@ const SignIn = () => {
         <CardFooter className="flex flex-col space-y-4">
           <div className="text-sm text-center text-muted-foreground">
             Don’t have an account?{" "}
-            <a href="/sign-up" className="underline underline-offset-4 hover:text-primary">
+            <a
+              href="/sign-up"
+              className="underline underline-offset-4 hover:text-primary"
+            >
               Sign up
             </a>
           </div>
         </CardFooter>
       </Card>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
