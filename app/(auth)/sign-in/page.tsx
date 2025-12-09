d709b864797ccb84e2c0ac637dbf8f355c237e42
Harsha Madushan <69555530+Harsha24798@users.noreply.github.com>
2025-12-09 11:29:30 +0530

    Implement authentication with NextAuth, add sign-in and sign-up routes, and integrate toast notifications

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,11 +1,29 @@
+'use client'
+
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Icons } from "@/components/icons"
+import { toast } from "sonner"
+import ROUTES from "@/constants/routes"
+import { signIn } from "next-auth/react"
 
 const SignIn = () => {
+
+  const handelSignIn = async (provider: "github" | "google") => {
+    try {
+      await signIn(provider, { 
+        callbackUrl: ROUTES.HOME,
+        redirect: true,
+      })
+    } catch (error) {
+      console.log(error)
+      toast.error("Something went wrong. Please try again.")
+    }
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center px-4 absolute">
       <Card className="w-3xl max-w-md">
@@ -17,7 +35,7 @@ const SignIn = () => {
         </CardHeader>
         <CardContent>
           <div className="grid grid-cols-2 gap-4 mb-6">
-            <Button variant="outline" className="w-full">
+            <Button variant="outline" className="w-full" onClick={() => handelSignIn("github")}>
               <Icons.github className="mr-2 h-4 w-4" />
               GitHub
             </Button>
@@ -32,7 +50,7 @@ const SignIn = () => {
               <span className="w-full border-t" />
             </div>
             <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground">Or continue with email</span>
+              <span className="bg-background px-2 text-muted-foreground pb-6">Or continue with email</span>
             </div>
           </div>
 
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -31,7 +31,7 @@ const SignUp = () => {
               <span className="w-full border-t" />
             </div>
             <div className="relative flex justify-center text-xs uppercase">
-              <span className="bg-background px-2 text-muted-foreground">Or sign up with email</span>
+              <span className="bg-background px-2 text-muted-foreground  pb-6">Or sign up with email</span>
             </div>
           </div>
 
